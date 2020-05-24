72a84e44106402c662094e90b4e9588abb53c237
yewonblairseo@gmail.com <yewonblairseo@gmail.com>
2020-05-24 16:44:16 +0200

    project done

--- public/login/login.js
+++ public/login/login.js
@@ -9,15 +9,14 @@ function validateLogin() {
     const username = document.forms.loginform.username.value;
     const password = document.forms.loginform.password.value;
 
-    if (username && password) {
-        return true;
-        // TODO: 이건 아니지.. 둘만 있으면 넘어가잖아. 얘때문에 안되는 거네. 비번이 8글자도 아닌데 그냥 넘기자나 ㅅㅂ
+    if (!username || !password) {
+        alert("Username or password should not be empty.");
+        return false;
     } else if (password.length < 8) {
         alert("Password should be more than 8 characters.");
         return false;
     } else {
-        alert("Username or password should not be empty.")
-        return false;
+        return true;
     }
 
 }
